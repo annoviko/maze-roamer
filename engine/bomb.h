cf6afa2628804270de1207eb8b39fac10f0be6de
annoviko <spb.andr@yandex.ru>
2023-06-07 21:59:46 +0200

    NO-REF: Update graphic related to bomb.

--- engine/bomb.h
+++ engine/bomb.h
@@ -8,7 +8,7 @@ public:
     bomb(const char p_id, const SDL_Rect& p_location, const position& p_logical_location, const texture_manager& p_manager, const int p_current_row = 1, const int p_current_frame = 0, const int p_num_of_frame = 0, const SDL_RendererFlip p_flip = SDL_FLIP_NONE) :
         game_object(p_id, p_location, p_logical_location, p_manager, p_current_row, p_current_frame, p_num_of_frame, p_flip)
     {
-        m_current_row = 6;
+        m_current_row = 7;
         m_current_frame = 6;
     }
 
--- engine/bomb_active.h
+++ engine/bomb_active.h
@@ -9,7 +9,7 @@ public:
     bomb_active(const char p_id, const SDL_Rect& p_location, const position& p_logical_location, const texture_manager& p_manager, const int p_current_row = 1, const int p_current_frame = 0, const int p_num_of_frame = 0, const SDL_RendererFlip p_flip = SDL_FLIP_NONE) :
         game_object_interim(p_id, p_location, p_logical_location, p_manager, 5000, p_current_row, p_current_frame, p_num_of_frame, p_flip)
     {
-        m_current_row = 6;
+        m_current_row = 7;
         m_current_frame = 7;
     }
 
@@ -17,7 +17,7 @@ public:
     void update() override { }
 
     void render() override {
-        m_current_frame = static_cast<int>((SDL_GetTicks() / 200) % 2) + 7;
+        m_current_frame = static_cast<int>((SDL_GetTicks() / 200) % 4) + 7;
         game_object::render();
     }
 };
