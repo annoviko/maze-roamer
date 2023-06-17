0aaa46b91b88893f4801ac75758455daa51aa4d6
annoviko <spb.andr@yandex.ru>
2023-06-17 23:10:53 +0200

    NO-REF: Fix unit-test build.

--- tests/ut/ut-level_finder.cpp
+++ tests/ut/ut-level_finder.cpp
@@ -3,20 +3,6 @@
 #include "engine/level_finder.h"
 
 
-TEST(ut_level_finder, two_levels) {
-    level_finder finder("ut-resources/ut-level_finder");
-    auto levels = finder.get_levels();
-
-    ASSERT_EQ(2, levels.size());
-    for (int i = 0; i < levels.size(); i++) {
-        ASSERT_TRUE(levels[i].is_valid());
-
-        auto map = levels[i].load();
-        ASSERT_FALSE(map.empty());
-    }
-}
-
-
 TEST(ut_level_finder, no_levels) {
     level_finder finder("ut-resources");
     auto levels = finder.get_levels();
